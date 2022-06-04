d022403629bdc1dadb15294dd24ff1348b79f3d5
Akshin Goswami <akshin@andi.health>
2022-06-04 17:26:34 +1000

    styled article screen

--- Screens/ArticleScreen.js
+++ Screens/ArticleScreen.js
@@ -2,14 +2,15 @@
     @brief: This component renders the article content for a seletced article
 */
 
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import React from 'react';
+import { ScrollView, Text, StyleSheet } from 'react-native';
 import {
   getSelectedArticleId,
   getArticleEntries,
 } from '../redux/articleCollectionSlice';
 import { useSelector } from 'react-redux';
 import FastImage from 'react-native-fast-image';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 // @params:
 //  articleId -> (string) contentfu article id used to load content.
@@ -22,31 +23,33 @@ export const ArticleScreen = () => {
     return entry.id === selectedArticleId;
   })[0];
 
-  useEffect(() => {
-    console.log('Article Selected: ', article);
-    console.log('thumbnail Url: ', article.thumbnailUrl);
-  });
   return (
-    <SafeAreaView style={styles.container}>
-      <FastImage
-        style={styles.articleThumbnail}
-        source={{
-          uri: article.thumbnailUrl,
-          priority: FastImage.priority.normal,
-          cache: FastImage.cacheControl.immutable,
-        }}
-      />
-      <Text> Loading Article: {article.title} </Text>
+    <SafeAreaView style={styles.safeContainer} edges={['top']}>
+      <ScrollView style={styles.scrollContainer}>
+        <FastImage
+          style={styles.articleThumbnail}
+          source={{
+            uri: article.thumbnailUrl,
+            priority: FastImage.priority.normal,
+            cache: FastImage.cacheControl.immutable,
+          }}
+        />
+        <Text> Loading Article: {article.title} </Text>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  safeContainer: {
     flex: 1,
     backgroundColor: '#942727',
     alignItems: 'center',
     flexDirection: 'column',
+  },
+  scrollContainer: {
+    width: '100%',
+    backgroundColor: '#f5f0ed',
   },
   articleThumbnail: {
     width: '100%',
