900ef10214000ba16fe56a5815359f8036970d3b
Akshin Goswami <akshin@andi.health>
2022-06-20 12:00:59 +1000

    enabled scrolling

--- Screens/ArticleScreen.js
+++ Screens/ArticleScreen.js
@@ -3,7 +3,13 @@
 */
 
 import React from 'react';
-import { ScrollView, Text, StyleSheet, View } from 'react-native';
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 import {
   getSelectedArticleId,
   getArticleEntries,
@@ -14,6 +20,18 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { RFPercentage } from 'react-native-responsive-fontsize';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 import RenderHTML, { defaultSystemFonts } from 'react-native-render-html';
+import { BLOCKS } from '@contentful/rich-text-types';
+
+// options for rich text render
+const options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      // this option adds the https portion of the string to the image url
+      // without it, images will not render.
+      `<img src="https:${node.data.target.fields.file.url}" alt="${node.data.target.fields.title}" />`;
+    },
+  },
+};
 
 // @params:
 //  articleId -> (string) contentfu article id used to load content.
@@ -25,8 +43,8 @@ export const ArticleScreen = () => {
   const article = entries.filter((entry) => {
     return entry.id === selectedArticleId;
   })[0];
-
-  console.log('body: ', article.body);
+  // window width for responsiveness
+  const { width } = useWindowDimensions();
 
   return (
     <SafeAreaView style={styles.safeContainer} edges={['top']}>
@@ -42,16 +60,17 @@ export const ArticleScreen = () => {
             cache: FastImage.cacheControl.immutable,
           }}
         />
-        <View style={styles.textContainer}>
+        <View style={styles.bodyContainer}>
           <Text style={styles.title}>{article.title}</Text>
           <Text style={styles.subtitle}>{article.subtitle}</Text>
-          <RenderHTML
-            baseStyle={styles.bodyContainer}
-            source={{
-              html: documentToHtmlString(article.body),
-            }}
-          />
         </View>
+        <RenderHTML
+          baseStyle={styles.bodyContainer}
+          source={{
+            html: documentToHtmlString(article.body, options),
+          }}
+          contentWidth={width - 20}
+        />
       </ScrollView>
     </SafeAreaView>
   );
@@ -68,7 +87,6 @@ const styles = StyleSheet.create({
   },
   scrollContentContainer: {
     alignItems: 'flex-start',
-    flex: 1,
     width: '100%',
   },
   articleThumbnail: {
